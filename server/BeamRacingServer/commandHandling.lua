b7dd1ebbaeaa3063b4fb769db5a97412ce300cbe
Joshua Manchester <joshuamanchester4@gmail.com>
2021-12-23 19:26:43 +0000

    Basic command handling using new event system

--- server/BeamRacingServer/commandHandling.lua
+++ server/BeamRacingServer/commandHandling.lua
@@ -1,5 +1,16 @@
-local M = {}
+local commandHandling = {}
 
+function HandleChatMessage(sender_id, sender_name, message)
 
+	print("Server received chat message from " .. sender_name .. " with id " .. sender_id .. ". Message is: " .. message)
+end
+
+function HandleConsoleInput(command)
+
+    print("Detected console input with command " .. command)
+end
+
+MP.RegisterEvent("onChatMessage", "HandleChatMessage")
+MP.RegisterEvent("onConsoleInput", "HandleConsoleInput")
 
 return M
--- server/BeamRacingServer/main.lua
+++ server/BeamRacingServer/main.lua
@@ -1,26 +1,10 @@
-local M = {}
+local main = {}
 
-function OnPlayerJoin(client)
-
-	print("Player just joined with ID " .. client)
-    --raceLogic.registerPlayer(client)
-end
-
-function OnPlayerDisconnect(client)
-
-	print("Player with ID " .. client .. " disconnected")
-	--raceLogic.deregisterPlayer(client)
-end
-
-function onInit()
+function OnInit()
 
 	print("BeamRacing initialized")
-
-	--RegisterEvent("onChatMessage", "OnChatMessage")
-	--RegisterEvent("onPlayerJoin", "OnPlayerJoin")
-	--RegisterEvent("onPlayerDisconnect", "OnPlayerDisconnect")
-
-	--raceLogic.onInit()
 end
 
-return M
+MP.RegisterEvent("onInit", "OnInit")
+
+return main
