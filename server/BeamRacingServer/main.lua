1f6f591550f876550d3b058255844a1c54007b28
Joshua Manchester <joshuamanchester4@gmail.com>
2021-12-23 18:26:43 +0000

    Started rewrite of main

--- server/BeamRacingServer/main.lua
+++ server/BeamRacingServer/main.lua
@@ -1,50 +1,26 @@
 local M = {}
 
-local raceLogic = require("/Resources/Server/BeamRacingServer/raceLogic")
-local clientControl = require("/Resources/Server/BeamRacingServer/clientControl")
-
 function OnPlayerJoin(client)
+
 	print("Player just joined with ID " .. client)
-    raceLogic.registerPlayer(client)
+    --raceLogic.registerPlayer(client)
 end
 
 function OnPlayerDisconnect(client)
+
 	print("Player with ID " .. client .. " disconnected")
-	raceLogic.deregisterPlayer(client)
-end
-
-function OnChatMessage(client, senderName, message)
-	message = message:sub(2)
-	if(message:sub(1, 1) == "/") then
-		local spacePos = message:find(" ")
-		local command, argument
-		if spacePos ~= nil then
-			command = message:sub(2, spacePos - 1)
-			argument = message:sub(spacePos + 1)
-		else
-			command = message:sub(2)
-			argument = ""
-		end
-
-		print("Command is:" .. command)
-		if command == "start" then
-			raceLogic.startRace(argument)
-		elseif command == "teleport" then
-			clientControl.teleportPlayer(client, argument)
-        elseif command == "quali" then
-			print("yo")
-			raceLogic.startQualifying(argument)
-		end
-	end
+	--raceLogic.deregisterPlayer(client)
 end
 
 function onInit()
-	print("Loaded BeamRacing")
-	RegisterEvent("onChatMessage", "OnChatMessage")
-	RegisterEvent("onPlayerJoin", "OnPlayerJoin")
-	RegisterEvent("onPlayerDisconnect", "OnPlayerDisconnect")
 
-	raceLogic.onInit()
+	print("BeamRacing initialized")
+
+	--RegisterEvent("onChatMessage", "OnChatMessage")
+	--RegisterEvent("onPlayerJoin", "OnPlayerJoin")
+	--RegisterEvent("onPlayerDisconnect", "OnPlayerDisconnect")
+
+	--raceLogic.onInit()
 end
 
 return M
